f5b843706394b0412383d64fabfd3feee33e0a21
AmrSayed0 <amracademycode@gmail.com>
2023-07-08 22:02:52 -0700

    [Updated] Fabric script to generate .tgz archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,20 +5,18 @@ from fabric.api import local
 from datetime import datetime
 
 def do_pack():
-    '''Generates .tgz archive from the contents of the web_static folder'''
-    local("mkdir -p versions")
-
-    # Check if 'tar' command exists
-    if local("which tar", capture=True).failed:
-        print("Error: 'tar' command not found.")
-        return None
-
-    archive_name = "web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    """Create a compressed archive from the web_static folder."""
+    now = datetime.now()
+    archive_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
     archive_path = "versions/{}".format(archive_name)
 
+    # Create versions folder if it doesn't exist
+    local("mkdir -p versions")
+
+    # Create the compressed archive
     result = local("tar -cvzf {} web_static".format(archive_path))
 
-    if result.failed:
-        return None
-    else:
+    if result.succeeded:
         return archive_path
+    else:
+        return None
